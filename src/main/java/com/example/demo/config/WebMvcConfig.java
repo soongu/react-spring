eb5e214c906b1bd0c7195b2046c155e529d86572
soongu <hsg9984@gmail.com>
2022-12-12 18:00:03 +0900

    #17. 개발설정 추가

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://http://todo-front.s3-website.ap-northeast-2.amazonaws.com")
+                .allowedOrigins("http://localhost:3000", "http://todo-front.s3-website.ap-northeast-2.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
